c6e42d8a1cbdcf9ee23017e0f82ba9df0e7231f4
ljj <893763507@qq.com>
2023-12-18 11:02:04 +0800

    update

--- cmd/main.go
+++ cmd/main.go
@@ -13,7 +13,8 @@ func main() {
 	mux.HandleFunc("/search", handle.Search)
 	mux.HandleFunc("/api/search", cos(handle.SearchService, "application/json;charset=UTF-8", http.MethodGet))
 	mux.HandleFunc("/api/stream", cos(handle.Stream, "application/octet-stream;charset=UTF-8", http.MethodPost))
-	mux.HandleFunc("/api/gemini", cos(handle.Geminiapi, "application/json;charset=UTF-8", http.MethodPost))
+	mux.HandleFunc("/api/gemini", cos(handle.Geminiapi, "application/octet-stream;charset=UTF-8", http.MethodPost))
+	mux.HandleFunc("/api/gemini1", cos(handle.Geminiapi, "application/json;charset=UTF-8", http.MethodPost))
 
 	server := &http.Server{
 		Addr:    "0.0.0.0:8080",
